4b29d482dbc074ff2499f6d5ce8724031d9b9761
Rajini <rajinipoosa@gmail.com>
2017-05-15 19:08:09 +0100

    started working on app.js file

---
+++ public/app.js
@@ -0,0 +1,27 @@
+var app = function(){
+// var Cat_ARRAY = [
+// { name: "Boba", favouriteFood:"Sock fluff",imageSrc:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
+//    { name: "Barnaby", favouriteFood:"Tuna",imageSrc:"http://dreamicus.com/data/cat/cat-01.jpg" },
+//     { name: "Max", favouriteFood:"Whiskas Temptations",imageSrc:"http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg" }];
+
+    var unOrderedList = document.createElement("ul");
+     unOrderedList.classList.add("cat");
+
+
+    // var name = document.createElement("li");
+    // name.innerText = "babby";
+
+    // var favouriteFood = document.createElement("li");
+    // favouriteFood.innerText = "fluff";
+
+    //  var img = document.createElement("li");
+    //  img.innerHTML = "<img src = http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg>";
+    //  favouriteFood.appendChild(img);
+    //   name.appendChild(favouriteFood);
+    //   unOrderedList.appendChild(name);
+    //  var cats = document.querySelector('#cats');
+    //  cats.appendChild(unOrderedList);
+};
+window.onload = app;
+
+   
